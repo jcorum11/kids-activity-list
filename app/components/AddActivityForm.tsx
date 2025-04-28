f3d4184b659504f3469de79e53388ac1957f899d
Jacob Corum <jacob.w.corum@gmail.com>
2025-04-28 15:58:26 -0400

    remove unused var

--- app/components/AddActivityForm.tsx
+++ app/components/AddActivityForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { Activity } from "@/lib/types";
 
 export default function AddActivityForm() {
   const [isOpen, setIsOpen] = useState(false);
